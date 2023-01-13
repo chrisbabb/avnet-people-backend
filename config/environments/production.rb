ee2cf229fe1eb7598b7557d2b8f381a9d9c0838e
Christopher Babb <chris@appsoftdev.com>
2023-01-12 20:52:25 -0700

    more fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options = {
+  host: 'https://oyster-app-7q899.ondigitalocean.app/avnet-people-backend'
+}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
