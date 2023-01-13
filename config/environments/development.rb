f2a7fa6e0165cfbea85d3786bac4ad10c5157cbd
Christopher Babb <chris@appsoftdev.com>
2023-01-12 18:55:55 -0700

    more fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: 'https://oyster-app-7q899.ondigitalocean.app/avnet-people-backend/'
+  host: 'https://oyster-app-7q899.ondigitalocean.app/avnet-people-backend'
 }
 
 Rails.application.configure do
